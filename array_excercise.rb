d7e091197474b1963b28736d54e71c67633f32a3
anne-other <anne.c.ince@gmail.com>
2018-07-18 22:20:14 +0100

    adding Glasgow Queen St to beginning  of stops array

--- array_excercise.rb
+++ array_excercise.rb
@@ -5,6 +5,9 @@ stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Ha
 stops.push("Edinburgh Waverley")
 
 # 2. Add "Glasgow Queen St" to the start of the array
+
+stops.unshift("Glasgow Queen St")
+
 # 3. Add "Polmont" at the appropriate point (between "Falkirk High" and "Linlithgow")
 # 4. Work out the index position of "Linlithgow"
 # 5. Remove "Livingston" from the array using its name
