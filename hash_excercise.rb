9bf6ecd238685c552ed35f9274d4ef448ec7145b
anne-other <anne.c.ince@gmail.com>
2018-07-18 23:18:13 +0100

    got Erik's hometown

--- hash_excercise.rb
+++ hash_excercise.rb
@@ -60,6 +60,9 @@ users = {
 p users["Jonathan"][:twitter]
 
 #2. Get Erik's hometown
+
+p users["Erik"][:home_town]
+
 #3. Get the array of Erik's lottery numbers
 #4. Get the type of Avril's pet Monty
 #5. Get the smallest of Erik's lottery numbers
