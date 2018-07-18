da9717ce7b7eb76935e954a7d7f6bcb7bec09c26
anne-other <anne.c.ince@gmail.com>
2018-07-18 23:27:32 +0100

    got Erik's smallest lottery number

--- hash_excercise.rb
+++ hash_excercise.rb
@@ -72,6 +72,10 @@ p users["Erik"][:lottery_numbers]
 p users["Avril"][:pets][0][:species]
 
 #5. Get the smallest of Erik's lottery numbers
+
+smallest_numbr = (users["Erik"][:lottery_numbers]).sort.shift
+p smallest_numbr
+
 #6. Return an array of Avril's lottery numbers that are even
 #7. Erik is one lottery number short! Add the number 7 to be included in his lottery numbers
 #8. Change Erik's hometown to Edinburgh
