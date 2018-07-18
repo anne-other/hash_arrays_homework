924f079fd5bb23586a097b253d0d2b26d0f40f35
anne-other <anne.c.ince@gmail.com>
2018-07-18 23:51:30 +0100

    add 7 to Erik's lottery numbers

--- hash_excercise.rb
+++ hash_excercise.rb
@@ -86,6 +86,10 @@ end
 p even_numbers
 
 #7. Erik is one lottery number short! Add the number 7 to be included in his lottery numbers
+
+users["Erik"][:lottery_numbers].push(7)
+p users["Erik"][:lottery_numbers]
+
 #8. Change Erik's hometown to Edinburgh
 #9. Add a pet dog to Erik called "Fluffy"
 #10. Add another person to the users hash
